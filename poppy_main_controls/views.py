b02322298634c4c9687547092a64625e55e5927e
Manon Cortial <mc@generationrobots.com>
2015-06-02 23:14:05 +0930

    corrected uwsgi.ini file read ankle position

--- poppy_main_controls/views.py
+++ poppy_main_controls/views.py
@@ -1,23 +1,34 @@
 from django.shortcuts import render
-#~ from poppy_humanoid import PoppyHumanoid
+from poppy_humanoid import PoppyHumanoid
 
-hasPoppy = False
+import time
 
+hasPoppy = True
 
+if hasPoppy:
+    try:
+        poppy = PoppyHumanoid()
+        time.sleep(0.1)
+    except Exception,e:
+            print "could not create poppy object"
+            print e
+    
 def index(request):
     context = {}
     context['other'] = request.POST
     
     
-    if hasPoppy:
-        poppy = PoppyHumanoid()
-    
+
+
     if 'compliant' in request.POST.keys():
         if hasPoppy:
-            poppy.compliant(True)
- 
+            #poppy.compliant =True
+            context['other'] = poppy.r_ankle_y.present_position
+            time.sleep(0.1)
+
     if 'notCompliant' in request.POST.keys():
         if hasPoppy:
-            poppy.compliant(False)
+            poppy.compliant = False
+            time.sleep(0.1)
 
     return render(request, 'poppy_main_controls/index.html',context )
